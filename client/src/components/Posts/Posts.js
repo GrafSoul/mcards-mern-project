65b1ebaacd66b2639f4ac3cc0011a23d5bf38ef4
GrafSoul <grafsoul@gmail.com>
2021-01-19 14:38:26 +0500

    feat: Added JSX Structure and Material UI library.

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,17 @@
+// Core
+import React from 'react';
+
+// Components
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post />
+            <Post />
+        </>
+    );
+};
+
+export default Posts;
