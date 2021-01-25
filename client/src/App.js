8c8befa777f74471ec4189d4d30dd37a2e28f1bd
GrafSoul <grafsoul@gmail.com>
2021-01-25 14:28:37 +0500

    feat: Added Auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 // Core
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 // Router
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 // Material UI
 import { Container } from '@material-ui/core';
 // Components
 import Navbar from './components/Navbar/Navbar.js';
+import Auth from './components/Auth/Auth.js';
 import Home from './components/Home/Home.js';
-
 // Styles
 import './index.css';
 
 const App = () => {
     return (
-        <Container maxWidth="lg">
-            <Navbar />
-            <Home />
-        </Container>
+        <BrowserRouter>
+            <Container maxWidth="lg">
+                <Navbar />
+                <Switch>
+                    <Route path="/" component={Home} />
+                    <Route path="/auth" component={Auth} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
     );
 };
 
