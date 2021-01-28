4cdba29a2ffbfce7ea1fcfd4755ed8e08664de60
GrafSoul <grafsoul@gmail.com>
2021-01-28 17:46:06 +0500

    feat: Added JWT tokens and a verification system using middleware.

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+// Core
+import jwt from 'jsonwebtoken';
+
+// Wants to like a post
+// Click the like button => auth middleware (next) => like controller...
+
+const auth = async (req, req, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const isCustomAuth = token.length < 500;
+        let decodedData;
+
+        if (token && isCustomAuth) {
+            decodedData = jwt.verify(token, 'test');
+            req.userId = decodedData?.id;
+        } else {
+            decodedData = jwt.decode(token);
+            req.userId = decodedData?.sub;
+        }
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default auth;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,6 @@
 // Core
 import express from 'express';
+
 // Controllers
 import {
     getPosts,
@@ -8,12 +9,16 @@ import {
     deletePost,
     likePost,
 } from '../controllers/posts.js';
+
+// Middleware
+import auth from '../middleware/auth';
+
 // Routes
 const router = express.Router();
 router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost);
-router.delete('/:id', deletePost);
-router.patch('/:id/likePost', likePost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
